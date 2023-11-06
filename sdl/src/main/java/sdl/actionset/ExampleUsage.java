e93674bdc5ef8be19782c0a62134332b28a0b9fa
Clayton Walker <cwalker@sofi.org>
2023-11-05 17:55:39 -0700

    Reformat

--- sdl/src/main/java/sdl/actionset/ExampleUsage.java
+++ sdl/src/main/java/sdl/actionset/ExampleUsage.java
@@ -9,9 +9,11 @@ public class ExampleUsage {
     class Action {
 
     }
+
     class Output {
 
     }
+
     static class ActionSet {
         private final String internalName;
         private final List<Action> actions;
@@ -26,29 +28,33 @@ public class ExampleUsage {
         public static ActionSet.Builder newBuilder() {
             return new ActionSet.Builder();
         }
+
         static class Builder {
             private String internalName;
             private List<Action> actions;
             private List<Output> outputs;
+
             public Builder internalName(String internalName) {
                 this.internalName = internalName;
                 return this;
             }
+
             public Builder actions(List<Action> actions) {
                 this.actions = actions;
                 return this;
             }
+
             public Builder outputs(List<Output> outputs) {
                 this.outputs = outputs;
                 return this;
             }
+
             public ActionSet build() {
-                return new ActionSet(internalName,
-actions
-                        , outputs);
+                return new ActionSet(internalName, actions, outputs);
             }
         }
     }
+
     public static void main(String[] args) {
         var sets = List.of(
                 ActionSet.newBuilder()
