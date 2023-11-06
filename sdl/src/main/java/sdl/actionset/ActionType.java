ba364ee89ca343127fdc6d1b1bdc96a7e8ec2971
Clayton Walker <clayton.m.walker@gmail.com>
2023-11-05 17:23:30 -0700

    Add initial support for actionsets

---
+++ sdl/src/main/java/sdl/actionset/ActionType.java
@@ -0,0 +1,12 @@
+package sdl.actionset;
+
+public enum ActionType {
+    Boolean,
+    IntegerRelative,
+    IntegerAbsolute,
+    UnsignedIntegerRelative,
+    UnsignedIntegerAbsolute,
+    FloatRelative,
+    FloatAbsolute,
+    ;
+}
---
+++ sdl/src/main/java/sdl/actionset/ExampleUsage.java
@@ -0,0 +1,61 @@
+package sdl.actionset;
+
+import java.util.List;
+
+/*
+https://gist.github.com/flibitijibibo/aa8a61196621adf17f76d182720830c1#chapter-0-what-are-action-sets
+ */
+public class ExampleUsage {
+    class Action {
+
+    }
+    class Output {
+
+    }
+    static class ActionSet {
+        private final String internalName;
+        private final List<Action> actions;
+        private final List<Output> outputs;
+
+        private ActionSet(String internalName, List<Action> actions, List<Output> outputs) {
+            this.internalName = internalName;
+            this.actions = actions;
+            this.outputs = outputs;
+        }
+
+        public static ActionSet.Builder newBuilder() {
+            return new ActionSet.Builder();
+        }
+        static class Builder {
+            private String internalName;
+            private List<Action> actions;
+            private List<Output> outputs;
+            public Builder internalName(String internalName) {
+                this.internalName = internalName;
+                return this;
+            }
+            public Builder actions(List<Action> actions) {
+                this.actions = actions;
+                return this;
+            }
+            public Builder outputs(List<Output> outputs) {
+                this.outputs = outputs;
+                return this;
+            }
+            public ActionSet build() {
+                return new ActionSet(internalName,
+actions
+                        , outputs);
+            }
+        }
+    }
+    public static void main(String[] args) {
+        var sets = List.of(
+                ActionSet.newBuilder()
+                        .internalName("Menus")
+                        .actions(List.of())
+                        .outputs(List.of())
+                        .build()
+        );
+    }
+}
